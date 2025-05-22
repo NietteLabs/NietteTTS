ec76d61338a2788d74b8ff7fb179df7e1b7d4e66
Pallas13 <edsondasilvaguedes0@gmail.com>
2025-05-22 20:04:42 -0300

    Script para converter e compilar dicionario

---
+++ dic/compile_dic.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Definido variaveis
+DIC_INPUT=$1
+
+echo "Converter léxico para formato puro do Festival"
+cut -f1 "$DIC_INPUT" | sed 's/^/( "/g' | tr "[:upper:]" "[:lower:]" > w.tmp
+cut -f2 "$DIC_INPUT" | sed 's/^/" nil (/g' | sed 's/$/))/g' > p.tmp
+
+paste w.tmp p.tmp -d" " | sed 's/ "/"/g' > "$(basename "$DIC_INPUT" .tsv).raw"
+
+echo "Compilar lexico"
+for i in "$(cat ../tools/utils.source)"; do
+export $i
+done
+
+$ESTDIR/../festival/bin/festival -b '(lex.compile "'$(basename "$DIC_INPUT" .tsv).raw'" "'$(basename "$DIC_INPUT" .tsv).out'")'
+
+echo "Limpar arquivos"
+
+rm p.tmp w.tmp
+
+
+
