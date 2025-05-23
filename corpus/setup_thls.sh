57153fc2ac2f414b859c18bce0afe9ed9fcfbcbf
Pallas13 <edsondasilvaguedes0@gmail.com>
2025-05-22 23:27:32 -0300

    Script para converter as transcrições do THLS em formato Festival.

---
+++ corpus/setup_thls.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm Pieroni/etc/txt.done.data 
+mkdir Pieroni/{etc,wav}
+iconv --from-code=ISO-8859-9 --to-code=UTF-8 1000-sentences-thls-dataset/sentences.txt > sentence_utf8.txt
+for i in $(seq -w 1 1000); do
+N=$(echo "$i"p)
+FRASE=$(sed -n "$N" 1000-sentences-thls-dataset/sentence_utf8.txt)
+echo "( THLS_PTBR_1000_FRA_$i \"$FRASE\" )" >> Pieroni/etc/txt.done.data
+done
+
+cp 1000-sentences-thls-dataset/Sentences/*.wav Pieroni/wav
+rm sentence_utf8.txt 
