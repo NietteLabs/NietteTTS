1d6dad1f4803b230ce0c0047ff6dc295aadf77a5
Pallas13 <edsondasilvaguedes0@gmail.com>
2025-06-02 02:49:06 -0300

    Adição de dicionaro separada com novas definições um (userdict) e mudanças de paramentros para obter resultados bons com g2p.

--- g2p/setup_g2p.sh
+++ g2p/setup_g2p.sh
@@ -5,10 +5,8 @@
 mkdir models/
 bash split.sh wikipron/data/scrape/tsv/por_latn_bz_broad_filtered.tsv models/train.dic models/test.dic
 #cat wikipron/data/scrape/tsv/por_latn_bz_broad_filtered.tsv > models/train.dic
-echo -e ".\tpau" >> models/train.dic
-echo -e ",\tsp" >> models/train.dic
-echo -e ";\tsp" >> models/train.dic
-echo -e "-\tsp" >> models/train.dic
+cat dic_add.tsv >> models/train.dic 
+
 # Rodar phonetisaurus de acordo com a arquitetura $(uname -m)
 ARCH=$(uname -m)
 #LD_LIBRARY_PATH=../tools/g2p/lib/$ARCH/
@@ -19,10 +17,11 @@ PATH=$PATH:$BIN_PATH
 # Fonte Original: https://github.com/AdolfVonKleist/Phonetisaurus
 
 echo "Alinhamento de Diciónario (5min-10min):"
-LD_LIBRARY_PATH=../tools/g2p/lib/$ARCH/ phonetisaurus-align --input=models/train.dic --ofile=models/train.corpus --seq1_del=false --seq2_del=false --iter=30 
+LD_LIBRARY_PATH=../tools/g2p/lib/$ARCH/ phonetisaurus-align --input=models/train.dic --ofile=models/train.corpus --seq1_del=false --seq2_del=false --nbest=4
 echo "Treinando modelo n-gram (5s-10s):" 
-#estimate-ngram -o 8 -t models/train.corpus -wl models/train.o8.arpa
-lmplz -o 8 < models/train.corpus > models/train.o8.arpa
+estimate-ngram -wv vocab -o 8 -t models/train.corpus -wl /tmp/ngram.tmp
+rm /tmp/ngram.tmp
+lmplz --collapse_values -o 8 --interpolate_unigrams 0 --limit_vocab_file vocab --discount_fallback < models/train.corpus > models/train.o8.arpa
 echo "Convertendo para formato OpenFst (10s-20s)":
 LD_LIBRARY_PATH=../tools/g2p/lib/$ARCH/ phonetisaurus-arpa2wfst --lm=models/train.o8.arpa --ofile=models/train.o8.fst
 
