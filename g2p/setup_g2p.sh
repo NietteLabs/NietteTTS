8ed21afc11734bdfff98d8803b79edfb4e56cadf
Pallas13 <edsondasilvaguedes0@gmail.com>
2025-06-05 02:28:02 -0300

    Modifições no parametros para obter resultado e adição do userdict

--- g2p/setup_g2p.sh
+++ g2p/setup_g2p.sh
@@ -5,7 +5,7 @@
 mkdir models/
 bash split.sh wikipron/data/scrape/tsv/por_latn_bz_broad_filtered.tsv models/train.dic models/test.dic
 #cat wikipron/data/scrape/tsv/por_latn_bz_broad_filtered.tsv > models/train.dic
-cat dic_add.tsv >> models/train.dic 
+cat userdict.tsv >> models/train.dic 
 
 # Rodar phonetisaurus de acordo com a arquitetura $(uname -m)
 ARCH=$(uname -m)
@@ -17,11 +17,11 @@ PATH=$PATH:$BIN_PATH
 # Fonte Original: https://github.com/AdolfVonKleist/Phonetisaurus
 
 echo "Alinhamento de Diciónario (5min-10min):"
-LD_LIBRARY_PATH=../tools/g2p/lib/$ARCH/ phonetisaurus-align --input=models/train.dic --ofile=models/train.corpus --seq1_del=false --seq2_del=false --nbest=4
+LD_LIBRARY_PATH=../tools/g2p/lib/$ARCH/ phonetisaurus-align --input=models/train.dic --ofile=models/train.corpus --seq1_del=false --seq2_del=false --nbest=8 --mbr=true --iter=40 --grow=true --fb=true
 echo "Treinando modelo n-gram (5s-10s):" 
-estimate-ngram -wv vocab -o 8 -t models/train.corpus -wl /tmp/ngram.tmp
+estimate-ngram -wv vocab -o 6 -t models/train.corpus -wl /tmp/ngram.tmp
 rm /tmp/ngram.tmp
-lmplz --collapse_values -o 8 --interpolate_unigrams 0 --limit_vocab_file vocab --discount_fallback < models/train.corpus > models/train.o8.arpa
+lmplz --discount_fallback --collapse_values -o 6 --limit_vocab_file vocab < models/train.corpus > models/train.o8.arpa
 echo "Convertendo para formato OpenFst (10s-20s)":
 LD_LIBRARY_PATH=../tools/g2p/lib/$ARCH/ phonetisaurus-arpa2wfst --lm=models/train.o8.arpa --ofile=models/train.o8.fst
 
--- g2p/setup_g2p_full.sh
+++ g2p/setup_g2p_full.sh
@@ -4,11 +4,8 @@
 # Criar arquivo de treinamento
 mkdir models/
 #bash split.sh wikipron/data/scrape/tsv/por_latn_bz_broad_filtered.tsv models/train.dic models/test.dic
-cat wikipron/data/scrape/tsv/por_latn_bz_broad_filtered.tsv | gawk '{print tolower($0)}' > models/train.dic
-echo -e ".\tpau" >> models/train.dic
-echo -e ",\tsp" >> models/train.dic
-echo -e ";\tsp" >> models/train.dic
-echo -e "-\tsp" >> models/train.dic
+cat wikipron/data/scrape/tsv/por_latn_bz_broad_filtered.tsv | gawk '{print tolower($0)}' > models/full.dic
+cat userdict.tsv >> models/full.dic
 # Rodar phonetisaurus de acordo com a arquitetura $(uname -m)
 ARCH=$(uname -m)
 #LD_LIBRARY_PATH=../tools/g2p/lib/$ARCH/
@@ -19,11 +16,11 @@ PATH=$PATH:$BIN_PATH
 # Fonte Original: https://github.com/AdolfVonKleist/Phonetisaurus
 
 echo "Alinhamento de Diciónario (5min-10min):"
-LD_LIBRARY_PATH=../tools/g2p/lib/$ARCH/ phonetisaurus-align --input=models/train.dic --ofile=models/train_full.corpus --seq1_del=false --seq2_del=false --nbest=4
+LD_LIBRARY_PATH=../tools/g2p/lib/$ARCH/ phonetisaurus-align --input=models/full.dic --ofile=models/train_full.corpus --seq1_del=false --seq2_del=false --nbest=8 --mbr=true --iter=40 --grow=true --fb=true
 echo "Treinando modelo n-gram (5s-10s):" 
-estimate-ngram -wv vocab -o 8 -t models/train_full.corpus -wl /tmp/ngram.tmp
+estimate-ngram -wv vocab -o 6 -t models/train_full.corpus -wl /tmp/ngram.tmp
 rm /tmp/ngram.tmp
-lmplz --collapse_values -o 8 --interpolate_unigrams 0 --limit_vocab_file vocab --discount_fallback < models/train_full.corpus > models/train_full.o8.arpa
+lmplz --discount_fallback --collapse_values -o 6 --limit_vocab_file vocab < models/train_full.corpus > models/train_full.o8.arpa
 echo "Convertendo para formato OpenFst (10s-20s)"
 LD_LIBRARY_PATH=../tools/g2p/lib/$ARCH/ phonetisaurus-arpa2wfst --lm=models/train_full.o8.arpa --ofile=models/train_full.o8.fst
 
