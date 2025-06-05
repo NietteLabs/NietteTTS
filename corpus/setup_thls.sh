f5b6597580aba2813475945d2bf568414de7a010
Pallas13 <edsondasilvaguedes0@gmail.com>
2025-06-05 18:43:32 -0300

    Normalização de texto

--- corpus/setup_thls.sh
+++ corpus/setup_thls.sh
@@ -5,7 +5,8 @@ mkdir Pieroni/{etc,wav}
 iconv --from-code=ISO-8859-9 --to-code=UTF-8 1000-sentences-thls-dataset/sentences.txt > sentence_utf8.txt
 for i in $(seq -w 1 1000); do
 N=$(echo "$i"p)
-FRASE=$(sed -n "$N" 1000-sentences-thls-dataset/sentence_utf8.txt | gawk '{print tolower($0)}')
+FRASE=$(sed -n "$N" 1000-sentences-thls-dataset/sentence_utf8.txt | gawk '{print tolower($0)}' | sed "s/\"/\'/g" | tr "-" " " | sed "s/ë/e/g" )
+echo $N
 echo "( THLS_PTBR_1000_FRA_$i \"$FRASE\" )" >> Pieroni/etc/txt.done.data
 done
 
