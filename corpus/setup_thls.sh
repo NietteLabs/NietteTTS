5e247159035e7f5daf82b83b6c63f4adeea4243c
Pallas13 <edsondasilvaguedes0@gmail.com>
2025-06-05 02:32:53 -0300

    Extrair as senteças do THLS para formato Festival e torna-las em minuculas.

--- corpus/setup_thls.sh
+++ corpus/setup_thls.sh
@@ -5,7 +5,7 @@ mkdir Pieroni/{etc,wav}
 iconv --from-code=ISO-8859-9 --to-code=UTF-8 1000-sentences-thls-dataset/sentences.txt > sentence_utf8.txt
 for i in $(seq -w 1 1000); do
 N=$(echo "$i"p)
-FRASE=$(sed -n "$N" 1000-sentences-thls-dataset/sentence_utf8.txt)
+FRASE=$(sed -n "$N" 1000-sentences-thls-dataset/sentence_utf8.txt | gawk '{print tolower($0)}')
 echo "( THLS_PTBR_1000_FRA_$i \"$FRASE\" )" >> Pieroni/etc/txt.done.data
 done
 
