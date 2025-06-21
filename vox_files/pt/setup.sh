9624a4dfb3c4649beeb78f8d4e5b5f0fc4d53ca7
Pallas13 <edsondasilvaguedes0@gmail.com>
2025-06-20 22:05:00 -0300

    Palavras passadas ao G2P vão ver convertidas para maiúscula

--- vox_files/pt/setup.sh
+++ vox_files/pt/setup.sh
@@ -3,7 +3,7 @@ LD_LIBRARY_PATH=festvox/g2p/lib/$ARCH/
 BIN_PATH=festvox/g2p/bin/$ARCH
 PATH=$PATH:$BIN_PATH
 
-WORD=$1
+WORD=$(echo $1 | gawk '{print tolower($0)}')
 MODEL_INPUT=festvox/g2p/model_g2p.fst
 PHONES=$(LD_LIBRARY_PATH=festvox/g2p/lib/$ARCH/ phonetisaurus-g2pfst --model="$MODEL_INPUT"  --word=$WORD | cut -f3)
 
