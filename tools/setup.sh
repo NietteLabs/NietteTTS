0984907908018363b21d0df5eb8ee9e637e1c6cb
Pallas13 <edsondasilvaguedes0@gmail.com>
2025-07-08 10:41:43 -0300

    Remoção do Flite

--- tools/setup.sh
+++ tools/setup.sh
@@ -6,8 +6,7 @@
 
 # Ubuntu
 echo "Instalando os pacotes necessários para as ferramentas"
-sleep 2
-sudo apt-get install git build-essential libncurses5-dev sox cmake csh doxygen xsltproc graphviz git g++ autoconf-archive make libtool python3-setuptools python-dev-is-python3 python3-pip gfortran jq gawk libboost-all-dev
+sudo apt-get install git build-essential libncurses5-dev sox cmake csh doxygen xsltproc graphviz git g++ autoconf-archive make libtool python3-setuptools python-dev-is-python3 python3-pip gfortran jq gawk libboost-all-dev python3-num2words
 # Pacotes python
 pip install sacrebleu jiwer --break-system-packages
 
@@ -18,7 +17,6 @@ wget http://festvox.org/packed/SPTK-3.6.tar.gz
 tar zxvf SPTK-3.6.tar.gz
 
 export ESTDIR=`pwd`/speech_tools
-export FLITEDIR=`pwd`/flite
 export FESTVOXDIR=`pwd`/festvox
 export SPTKDIR=`pwd`/SPTK
 
@@ -44,11 +42,6 @@ make default_voices
 cd ..
 
 cd festvox
-./configure
-make
-cd ..
-
-cd flite
 ./configure
 make
 cd ..
@@ -80,7 +73,6 @@ cd ../../
 
 ## Salva as variáveis das ferramentas em um arquivo
 echo "ESTDIR=$ESTDIR" >> utils.source
-echo "FLITEDIR=$FLITEDIR" >> utils.source
 echo "FESTVOXDIR=$FESTVOXDIR" >> utils.source
 echo "SPTKDIR=$SPTKDIR" >> utils.source
 echo "REAPER=`pwd`/REAPER/build/reaper" >> utils.source
