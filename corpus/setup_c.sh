5286a7dd0c485dbb322ebd2d724d1a7f17f38b80
Pallas13 <edsondasilvaguedes@gmail.com>
2025-10-05 19:21:14 -0300

    Update corpus/Pieroni/etc/txt.done.data Transcrições com palavras com hifer.

--- corpus/setup_c.sh
+++ corpus/setup_c.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
-#for i in $(ls *.wav); do fname=$(basename $i .wav); n=$(echo $fname | cut -f2 -d"-"); echo $n;  n_1=$(echo "$n + 0" | bc);  mv F017-$n.wav F017-$n_1.wav; done
+# Converte CETUC DATASET para formato
+make_txt() {
+    ID_SPEAK="$1"
+    for i in $(seq -w 1 1000); do
+        N=$(echo "$i"p)
+        FRASE=$(sed -n "$N" sentences_thls_utf8.txt)
+        N=$(echo "$i - 1" | bc)
+        echo "( $ID_SPEAK-$N \"$FRASE\" )" >>"$ID_SPEAK".txt
+    done
+}
 
-iconv --from-code=ISO-8859-9 --to-code=UTF-8 1000-sentences-thls-dataset/sentences.txt | gawk '{print tolower($0)}' | sed "s/\"/\'/g" | tr "-" " " | sed "s/ë/e/g" > 1000-sentences-thls-dataset/sentence_utf8.txt
-for i in $(seq -w 1 1000); do
-N=$(echo "$i"p)
-FRASE=$(sed -n "$N" 1000-sentences-thls-dataset/sentence_utf8.txt)
-N=$(echo "$i - 1" | bc)
-echo "( F050-$N \"$FRASE\" )"
-done
+rename_wavs() {
+    DIR_CORPUS=$1
+    for speak in $DIR_CORPUS/*; do
+echo $speak
+        for i in $speak/*.wav; do
+            fname=$(basename $i .wav)
+            ID_SPEAK=$(echo "$fname" | cut -f1 -d"-")
+	    echo $fname
+            n=$(echo "$fname" | cut -f2 -d"-")
+            n_1=$(echo "$n + 0" | bc)
+	    FRASE=$(cat $speak/$ID_SPEAK-$n.txt)
+	    echo "( $ID_SPEAK-$n_1 \"$FRASE\" )" >>"$ID_SPEAK".txt
+            INPUT=$speak/$ID_SPEAK-$n.wav
+            OUTPUT=$speak/$ID_SPEAK-$n_1.wav
+            mv "$INPUT" "$OUTPUT"
+        done
+    done
+}
 
-#cp 1000-sentences-thls-dataset/Sentences/*.wav Pieroni/wav
-#rm sentence_utf8.txt 
+rename_wavs $1
--- corpus/setup_thls.sh
+++ corpus/setup_thls.sh
@@ -2,7 +2,7 @@
 
 rm Pieroni/etc/txt.done.data 
 mkdir Pieroni/{etc,wav}
-iconv --from-code=ISO-8859-9 --to-code=UTF-8 1000-sentences-thls-dataset/sentences.txt | gawk '{print tolower($0)}' | sed "s/\"/\'/g" | tr "-" " " | sed "s/ë/e/g" > 1000-sentences-thls-dataset/sentence_utf8.txt
+iconv --from-code=ISO-8859-9 --to-code=UTF-8 1000-sentences-thls-dataset/sentences.txt | gawk '{print tolower($0)}' | sed "s/\"/\'/g" | sed "s/ë/e/g" > 1000-sentences-thls-dataset/sentence_utf8.txt
 for i in $(seq -w 1 1000); do
 N=$(echo "$i"p)
 FRASE=$(sed -n "$N" 1000-sentences-thls-dataset/sentence_utf8.txt)
